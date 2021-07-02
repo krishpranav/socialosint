dc91f2b2fb84fe1b39000a677b3d207c297af9ca
krishpranav <krisna.pranav@gmail.com>
2021-07-02 10:16:15 +0530

    [+] get cookies file path [+]

--- lib/LinkedinAPI/cookie_repository.py
+++ lib/LinkedinAPI/cookie_repository.py
@@ -21,3 +21,16 @@ class CookieRepository(object):
         cookiejar_filepath = CookieRepository._get_cookies_filepath(username)
         with open(cookiejar_filepath, "wb") as f:
             pickle.dump(cookies, f)
+
+    @staticmethod
+    def get(username):
+        cookies = CookieRepository._load_cookies_from_cache(username)
+        if cookies and not CookieRepository._is_token_still_valid(cookies):
+            raise LinkedinSessionExpired
+
+        return cookies
+
+    @staticmethod
+    def _get_cookies_filepath(username):
+        if not os.path.exists(settings.COOKIE_PATH):
+            os.makedirs(settings.COOKIE_PATH)
