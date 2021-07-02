a90ffbc99d99200751bfb5a1cfa03a69d1cd04a5
krishpranav <krisna.pranav@gmail.com>
2021-07-02 10:23:39 +0530

    [+] init [+]

---
+++ lib/LinkedinAPI/__init__.py
@@ -0,0 +1,10 @@
+from .linkedin import Linkedin
+
+__title__ = "linkedin_api"
+__version__ = "1.0.0"
+__description__ = "Python wrapper for linkeding api"
+
+__author__ = "Tom Quirk"
+__email__ = "tomquirkacc@gmail.com"
+
+__all__ = ["Linkedin"]
