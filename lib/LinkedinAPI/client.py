ac335e2487158c06cdcb5c33f2139163c73123fb
krishpranav <krisna.pranav@gmail.com>
2021-07-02 10:18:23 +0530

    [+] logger [+]

---
+++ lib/LinkedinAPI/client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports
+import requests
+import logging
+from .cookie_repository import CookieRepository
+
+logger.getLogger("requests").setLevel(logging.INFO)
+logger.getLogger("requests").setLevel(logging.DEBUG)
+logger.getLogger("requests").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
