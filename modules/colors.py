9791079f93d0ea5f6d9e1594099eb8fbfe8a6b01
krishpranav <krisna.pranav@gmail.com>
2021-07-02 10:05:00 +0530

    [+] colors [+]

--- modules/colors.py
+++ modules/colors.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 class colors:
-    
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     G, Y, R, W, V, B, end = '\033[92m', '\033[93m', '\033[91m', '\x1b[37m', '\033[95m', '\033[94m', '\033[0m'
