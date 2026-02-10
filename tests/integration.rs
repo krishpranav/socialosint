49015d49039c25f0d8bf275394faa631b5deaf65
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2026-02-10 19:17:47 +0530

    tests: integration

---
+++ tests/integration.rs
@@ -0,0 +1,10 @@
+mod common;
+
+#[path = "integration/instagram_tests.rs"]
+mod instagram_tests;
+
+#[path = "integration/twitter_tests.rs"]
+mod twitter_tests;
+
+#[path = "integration/pwndb_tests.rs"]
+mod pwndb_tests;
